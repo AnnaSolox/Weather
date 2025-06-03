5facaa1aae81dc17cf6ea384572455dd5cc71b7f
AnnaSolox <anna.sol.loz@gmail.com>
2025-06-03 13:36:17 +0200

    repository io dispatcher

--- app/src/main/java/com/annasolox/weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/annasolox/weather/data/repository/WeatherRepository.kt
@@ -7,30 +7,34 @@ import com.annasolox.weather.data.bbdd.model.City
 import com.annasolox.weather.ui.mapper.ForecastMapper
 import com.annasolox.weather.ui.mapper.WeatherMapper
 import com.annasolox.weather.ui.model.Weather
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(
     private val weatherApiService: OpenWeatherApiService,
     private val cityDao: CityDao
 ) {
-    suspend fun getWeather(lat: Double, lon: Double, language: String): Resource<Weather> {
-        return try {
-            val currentWeather = weatherApiService.getCurrentWeather(lat, lon, language)
-            val forecast = weatherApiService.getForecastWeather(lat, lon, language)
+    suspend fun getWeather(lat: Double, lon: Double, language: String): Resource<Weather> =
+        withContext(Dispatchers.IO) {
+            try {
+                val currentWeather = weatherApiService.getCurrentWeather(lat, lon, language)
+                val forecast = weatherApiService.getForecastWeather(lat, lon, language)
 
-            val weatherUi = WeatherMapper.fromApiToUi(currentWeather)
-            val forecastUi = ForecastMapper.fromApiToUi(forecast)
-            weatherUi.forecast = forecastUi
+                val weatherUi = WeatherMapper.fromApiToUi(currentWeather)
+                val forecastUi = ForecastMapper.fromApiToUi(forecast)
+                weatherUi.forecast = forecastUi
 
-            Resource.Success(weatherUi)
-        } catch (e: Exception) {
-            Resource.Error(e)
+                Resource.Success(weatherUi)
+            } catch (e: Exception) {
+                Resource.Error(e)
+            }
         }
-    }
 
     fun getCitiesByName(
         name: String,
@@ -41,4 +45,5 @@ class WeatherRepository @Inject constructor(
             .map<List<City>, Resource<List<City>>> { Resource.Success(it) }
             .onStart { emit(Resource.Loading) }
             .catch { e -> emit(Resource.Error(e)) }
+            .flowOn(Dispatchers.IO)
 }
